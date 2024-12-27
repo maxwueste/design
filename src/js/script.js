85776104b2812f260aa3d3551c6959148e137cbe
maxwueste <boerries@gmx.net>
2024-12-27 20:42:26 +0100

    developing index.html and depending data

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+import {
+  animate,
+  scroll,
+} from "https://cdn.jsdelivr.net/npm/framer-motion@11.11.11/dom/+esm";
+
+const items = document.querySelectorAll("li");
+
+// Animate gallery horizontally during vertical scroll
+scroll(
+  animate(
+    "ul",
+    {
+      transform: ["none", `translateX(-${items.length - 1}00vw)`],
+    },
+    { ease: "linear" }
+  ),
+  { target: document.querySelector("section") }
+);
+
+// Progress bar representing gallery scroll
+scroll(animate(".progress", { scaleX: [0, 1] }, { ease: "linear" }), {
+  target: document.querySelector("section"),
+});
+
+// Image title parallax
+const segmentLength = 1 / items.length;
+items.forEach((item, i) => {
+  const header = item.querySelector("h3");
+
+  scroll(animate(header, { x: [500, -100] }, { ease: "linear" }), {
+    target: document.querySelector("section"),
+    offset: [
+      [i * segmentLength, 1],
+      [(i + 1) * segmentLength, 0],
+    ],
+  });
+});
